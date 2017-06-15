485570c3f13c7938acdc203213f04115b3b25f08
Richard H Fung <contact@rhfung.com>
2017-06-14 21:41:41 -0700

    make compatible with webpack

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-// Support AMD in the browser and CommonJS (normal) require in Node.  Use this
-// snippet so that this code will be stripped out when using the RequireJS optimizer.
-if (typeof define !== 'function') {
-  var define = require('amdefine')(module);
-}
-
-define(function () {
+;(function (global, factory) {
+    typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+    typeof define === 'function' && define.amd ? define(factory) :
+    global.rfc822_validate = factory()
+}(this, function () {
+  'use strict';
   var sQtext = '[^\\x0d\\x22\\x5c\\x80-\\xff]';
   var sDtext = '[^\\x0d\\x5b-\\x5d\\x80-\\xff]';
   var sAtom = '[^\\x00-\\x20\\x22\\x28\\x29\\x2c\\x2e\\x3a-\\x3c\\x3e\\x40\\x5b-\\x5d\\x7f-\\xff]+';
@@ -26,4 +25,5 @@ define(function () {
   }
 
   return f;
-});
+}));
+
